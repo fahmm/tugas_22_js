17ca2af5dd17f13527f92e4c34931f2194f29f1e
fahmi <pammimajid@gmail.com>
2020-03-19 16:56:15 +0800

    ok

---
+++ tugas_22_js.js
@@ -0,0 +1,8 @@
+function tugas22(){
+var a = "Saya ingin belajar bersama"
+var b = a.split(" ")
+b.forEach(function(item,index,array){
+	console.log("item : "+item+" index ke "+index )
+})
+}
+tugas22 ()
